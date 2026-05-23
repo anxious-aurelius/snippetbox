9d7b045aaf17c0fcf987ed1195bbbf45151787ec
Kripal Parsekar <kripalparsekar@gmail.com>
2026-05-23 18:18:38 +0100

    Restructured the project directory

---
+++ cmd/web/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /{$}", home)
+	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
+	mux.HandleFunc("GET /snippet/create", snippetCreate)
+	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
+
+	log.Print("starting server on port :4001")
+	err := http.ListenAndServe("127.0.0.1:4001", mux)
+	log.Fatal(err)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/anxious-aurelius/url-shortener
+
+go 1.25.6
