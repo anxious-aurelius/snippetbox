8df144e850ea20c2aa6f71bd9dd8ae4eacc15be8
Kripal Parsekar <kripalparsekar@gmail.com>
2026-05-23 20:45:16 +0100

    Homepage constructed using templates. Added static files

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,19 +2,41 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
+
 	w.Header().Add("Server", "Go")
-	w.Write([]byte("Hello from Snippetbox"))
+
+	files := []string{
+		"./ui/html/base.html",
+		"./ui/html/pages/home.html",
+		"./ui/html/partials/nav.html",
+	}
+
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	err = ts.ExecuteTemplate(w, "base", nil)
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
 }
 
 func snippetView(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.PathValue("id"))
 	if err != nil || id < 1 {
 		http.NotFound(w, r)
+		return
 	}
 
 	fmt.Fprintf(w, "Display a specific snippet with ID %d", id)
