65f54618408d0b602c59ff6e8481951b2096cabe
Kripal Parsekar <kripalparsekar@gmail.com>
2026-05-23 22:26:02 +0100

    added styling and fileserver

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -6,11 +6,16 @@ import (
 )
 
 func main() {
+
+	fileserver := http.FileServer(http.Dir("./ui/static"))
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("GET /{$}", home)
 	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
 	mux.HandleFunc("GET /snippet/create", snippetCreate)
 	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
+
+	mux.Handle("GET /static/", http.StripPrefix("/static", fileserver))
 
 	log.Print("starting server on port :4001")
 	err := http.ListenAndServe("127.0.0.1:4001", mux)
