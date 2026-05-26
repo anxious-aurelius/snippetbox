2729e8a5b96fa2841223fbf9df50054c9b01286b
Kripal Parsekar <kripalparsekar@gmail.com>
2026-05-26 23:39:29 +0100

    added helper error handling functions and fixed deliberate error

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -17,21 +17,20 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Server", "Go")
 
 	files := []string{
+		"./ui/html/base.html",
 		"./ui/html/pages/home.html",
 		"./ui/html/partials/nav.html",
 	}
 
 	ts, err := template.ParseFiles(files...)
 	if err != nil {
-		app.logger.Error(err.Error(), "method", r.Method, "url", r.URL.RequestURI())
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		app.serverError(w, r, err)
 		return
 	}
 
 	err = ts.ExecuteTemplate(w, "base", nil)
 	if err != nil {
-		app.logger.Error(err.Error(), "method", r.Method, "url", r.URL.RequestURI())
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		app.serverError(w, r, err)
 	}
 
 
