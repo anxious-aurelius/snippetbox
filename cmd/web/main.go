db9c5febc12f9c18de45ed1517df19fc48ae8afd
Kripal Parsekar <kripalparsekar@gmail.com>
2026-05-27 23:21:38 +0100

    moved routes to a seperate file

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -21,19 +21,9 @@ func main() {
 		logger: logger,
 	}
 
-	fileserver := http.FileServer(http.Dir("./ui/static"))
-
-	mux := http.NewServeMux()
-	mux.HandleFunc("GET /{$}", app.home)
-	mux.HandleFunc("GET /snippet/view/{id}", app.snippetView)
-	mux.HandleFunc("GET /snippet/create", app.snippetCreate)
-	mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
-
-	mux.Handle("GET /static/", http.StripPrefix("/static", fileserver))
-
 	logger.Info("starting server","addr", *addr)
 
-	err := http.ListenAndServe(*addr, mux)
+	err := http.ListenAndServe(*addr, app.routes())
 	logger.Error(err.Error())
 	os.Exit(1)
 }
